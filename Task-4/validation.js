c0f2038a6c779fdecc0546d9da60138eaa8800c4
HastiDhola09 <hastidhola09@gmail.com>
2024-07-11 21:39:50 +0530

    Add files via upload

---
+++ Task-4/validation.js
@@ -0,0 +1,14 @@
+function validateLogin() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var message = document.getElementById('loginMessage');
+
+    // You can customize your authentication logic here
+    if (username === 'user' && password === 'password') {
+        message.style.color = 'green';
+        message.textContent = 'Login successful!';
+    } else {
+        message.style.color = 'red';
+        message.textContent = 'Invalid username or password.';
+    }
+}
